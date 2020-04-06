75c83be8ac032b6b9cfd173721b1eb3b90e4bd83
andreasjost <codefactory@gmx.ch>
2020-04-06 09:31:05 +0000

    Add environment variable, connect 'run.py' to the MongoDB Atlas database

--- run.py
+++ run.py
@@ -1,7 +1,20 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+from os import path
 
 app = Flask(__name__)
+
+if path.exists("env.py"):
+    import env
+
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.config["MONGO_DBNAME"] = 'software_forum'
+
+
+mongo = PyMongo(app)
 
 
 @app.route('/')
