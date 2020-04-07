44600432c6bdc0b5e593e1c7188d57cec32e9085
andreasjost <codefactory@gmx.ch>
2020-04-07 15:13:09 +0000

    Added file 'addtopic.html' and extended 'run.py' to add functionality of saving a new topic to the database

--- run.py
+++ run.py
@@ -20,12 +20,22 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/get_topics')
 def get_topics():
-    return render_template("topics.html", topics=mongo.db.topics.find())
+    return render_template("topics.html",
+        topics=mongo.db.topics.find(),
+        answers=mongo.db.answers.find())
 
 
 @app.route('/add_topic')
-def add_task():
+def add_topic():
     return render_template('addtopic.html')
+
+
+@app.route('/insert_topic', methods=['POST'])
+def insert_topic():
+    topics = mongo.db.topics
+    topics.insert_one(request.form.to_dict())
+    print(request.form.to_dict())
+    return redirect(url_for('get_topics'))
 
 
 if __name__ == '__main__':
